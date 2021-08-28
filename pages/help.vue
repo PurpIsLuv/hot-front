513143b53012d1db515302cedcf2d678037a350b
Kim <yundunov@amigoweb.ru>
2021-08-28 21:48:19 +0700

    upd text in help page

--- pages/help.vue
+++ pages/help.vue
@@ -2,10 +2,8 @@
   <div>
     <h2>Help</h2>
     <p class="text-body-1">
-      Do you think that the site contains photos or videos that violate the law, social moral principles, etc.? Write about this to the website administrator by email pr.sexus@gmail.com! But above all, please note that we comply with 18 U.S.C. 2257 RKRCS, according to which, all persons (actors, actresses) have reached the age of 18 at the time of filming, and all porn videos published on this website are staged, the authors (studio url) are indicated on each video page.
+      Help our and your site! If you decide to donate to us, it will mean the world to us! All donations will go a long way in helping us pay for servers and upgrade site!
     </p>
-    <p class="text-body-1">
-      Dear copyright holders, if your works (videos, images) are posted on this website and you want to delete them, or if your contact information is incorrectly indicated, then you can contact the site administrator by mail pr.sexus@gmail.com. I assure you, no one will ignore these letters and we will try to figure it out.
-    </p>
+    <p>Paypal: sexus.prod@gmail.com</p>
   </div>
 </template>
